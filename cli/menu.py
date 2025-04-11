8544f410bfdcc11812dbe2702fa8ae9ccbfc99e9
Ricardo Pari <rikiricardo16@hotmail.com>
2025-04-11 09:01:27 -0400

    [11-April-2025] add init menu and main.py

---
+++ cli/menu.py
@@ -0,0 +1,10 @@
+def show_menu():
+
+    while True:
+        print("\n--- COVID Image Management CLI ---")
+        print("1. Download images from Kaggle")
+        print("3. List images by category")
+        print("4. Add new image")
+        print("5. Delete image")
+        print("6. Rename image")
+        print("7. Exit")
---
+++ main.py
@@ -0,0 +1,6 @@
+# Import the main menu from the CLI module
+from cli.menu import show_menu
+
+# Entry point of the program
+if __name__ == "__main__":
+    show_menu()
